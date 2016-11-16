5e4bbe0e4805ae0e3b996661e541ffd4c24bec19
Benoit Blanchon <github@benoitblanchon.fr>
2016-11-16 21:39:10 +0100

    Appveyor: add mingw

---
+++ scripts/appveyor/mingw64.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set PATH=C:\mingw64\bin;%PATH:C:\Program Files\Git\usr\bin;=%
+set CONFIGURATION=Debug
+
+cmake -DCMAKE_BUILD_TYPE=%CONFIGURATION% -G "MinGW Makefiles" . || exit /b 1
+cmake --build . || exit /b 2
+ctest -V . || exit /b 3
